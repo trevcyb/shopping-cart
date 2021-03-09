90c88b239dc4e793dd20e6f3446f4138f46ad73c
trevcyb <trevontewari@gmail.com>
2021-03-08 21:55:05 -0400

    added nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Shop from "./Components/Shop";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
 import "./Styles/App.css";
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Shop />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route path="/shop" component={Shop} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div className="container">
+            <h2>Welcome to the Shop!</h2>
+            <p>We here are the Shop are dedicated to providing nothing but the best quality items for you and yours! Happy Shopping!</p>
+        </div>
+    )
+}
+
+export default Home
